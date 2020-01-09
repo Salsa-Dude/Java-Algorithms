97b33c3f03530676cc3d3e38c32649f4d05ed9cd
Salsa-Dude <arias.joseph.a@gmail.com>
2020-01-09 16:43:15 -0500

    added another solution for palindrome number

--- palindrome-number/palindrome-number/Main.java
+++ palindrome-number/palindrome-number/Main.java
@@ -13,27 +13,57 @@ public class Main {
 		if(x < 0) {
 			return false;
 		}
-        
-		String newString = "";
-		// This method returns the string representation of the passed argument.
-		String stringNum = String.valueOf(x);
+			
+		int reverseNum = 0;
+		int copyX = x;
 		
-		for(int i = stringNum.length() - 1; i >= 0; i--) {
-			//charAt(index) method returns the character at the specified index. 
-			newString += stringNum.charAt(i);
+		
+		while(copyX != 0) {
+			// get the last digit of copyX
+			int lastDigit = copyX % 10;
+			
+			// multiply reverseNum by 10 to update the place value
+			reverseNum *= 10;
+			
+			reverseNum += lastDigit;
+			// downgrade copyX place value
+			copyX /= 10;
 		}
 		
-		System.out.println(Long.valueOf(newString));
-		System.out.println(Long.valueOf(x));
 		
+		return reverseNum == x;
 		
-		
-		if(Long.valueOf(newString).equals(Long.valueOf(x))) {
-			return true;
-		} else {
-			return false;
-		}
 		
 	}
+	
+	// Converting number into a string 
+	
+//	public static boolean isPalindrome(int x) {
+//		
+//		if(x < 0) {
+//			return false;
+//		}
+//        
+//		String newString = "";
+//		// This method returns the string representation of the passed argument.
+//		String stringNum = String.valueOf(x);
+//		
+//		for(int i = stringNum.length() - 1; i >= 0; i--) {
+//			//charAt(index) method returns the character at the specified index. 
+//			newString += stringNum.charAt(i);
+//		}
+//		
+//		System.out.println(Long.valueOf(newString));
+//		System.out.println(Long.valueOf(x));
+//		
+//		
+//		
+//		if(Long.valueOf(newString).equals(Long.valueOf(x))) {
+//			return true;
+//		} else {
+//			return false;
+//		}
+//		
+//	}
 
 }
