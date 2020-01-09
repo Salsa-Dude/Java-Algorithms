fa81ddb57b53a32357b96aa9882df209daa3beb0
Salsa-Dude <arias.joseph.a@gmail.com>
2020-01-09 15:36:55 -0500

    completed palindrome number

--- palindrome-number/palindrome-number/Main.java
+++ palindrome-number/palindrome-number/Main.java
@@ -1,9 +1,39 @@
+import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hey");
+		System.out.println(isPalindrome(313));
+		
+	}
+	
+	public static boolean isPalindrome(int x) {
+		
+		if(x < 0) {
+			return false;
+		}
+        
+		String newString = "";
+		// This method returns the string representation of the passed argument.
+		String stringNum = String.valueOf(x);
+		
+		for(int i = stringNum.length() - 1; i >= 0; i--) {
+			//charAt(index) method returns the character at the specified index. 
+			newString += stringNum.charAt(i);
+		}
+		
+		System.out.println(Long.valueOf(newString));
+		System.out.println(Long.valueOf(x));
+		
+		
+		
+		if(Long.valueOf(newString).equals(Long.valueOf(x))) {
+			return true;
+		} else {
+			return false;
+		}
+		
 	}
 
 }
