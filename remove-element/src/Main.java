6af18fb5b26fc6ed37980b7b7e6405781f9135c9
Joseph Arias <arias.joseph.a@gmail.com>
2020-01-13 20:40:26 -0500

    completed remove element

--- remove-element/src/Main.java
+++ remove-element/src/Main.java
@@ -1,6 +1,28 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int[] nums = {3,2,2,3};
+        removeElement(nums, 3);
+    }
+
+    public static int removeElement(int[] nums, int val) {
+
+        if(nums.length == 0) return 0;
+
+        // slow pointer
+        // going to represent the beginning of the array
+        int i = 0;
+
+        for(int j = 0; j < nums.length; j++) {
+            // if current nums[j] doesn't match the val
+           if(nums[j] != val) {
+               // store it in the beginning of the array which is what the slow pointer is representing.
+               nums[i] = nums[j];
+               // increment the slow pointer index by +1
+               i++;
+           }
+        }
+
+        return i + 1;
     }
 }
